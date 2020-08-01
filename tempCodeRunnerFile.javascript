face992b5590736fb40abdd9f2757944b4901143
Kumar Sambhav <krsambhav16@gmail.com>
2020-08-02 04:39:25 +0530

    .

---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,4 @@
+let xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+xhttp.send();
--- wish.py
+++ wish.py
@@ -1,3 +1,4 @@
+import time
 import datetime
 import json
 import urllib3
@@ -17,6 +18,7 @@ def check_users(token):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
     r = http.request('GET', url)
     dec_resp = json.loads(r.data.decode('utf-8'))
+    print('Getting User Data, Response ---> ', r.status)
     lenId = len(dec_resp['result'])
     userArr = []
     nameArr = []
@@ -27,6 +29,7 @@ def check_users(token):
         nameArr.append(name)
     userArr = list(dict.fromkeys(userArr))
     nameArr = list(dict.fromkeys(nameArr))
+    print(userArr, nameArr)
     return userArr, nameArr
 
 
@@ -39,13 +42,15 @@ def send_wish(msg, userList, nameList, token):
             '/sendMessage?chat_id=' + \
             str(userList[x]) + '&parse_mode=markdown&text=*Good Morning ' + \
             str(nameList[x]) + '!*' + msg
-        response = http.request('GET', send_text)
+        r = http.request('GET', send_text)
+        print(f'Wishing {nameList[x]}, Response ---> {r.status}')
 
 
 def get_covid_data():
     url = 'https://api.covid19india.org/data.json'
     r = http.request('GET', url)
     dec_resp = json.loads(r.data.decode('utf-8'))
+    print('Getting CoViD Data, Response ---> ', r.status)
     total_confirmed = int(dec_resp['statewise'][0]['confirmed'])
     total_active = int(dec_resp['statewise'][0]['active'])
     total_recovered = int(dec_resp['statewise'][0]['recovered'])
@@ -68,11 +73,13 @@ def get_quote():
     url = 'https://type.fit/api/quotes'
     r = http.request('GET', url)
     dec_resp = json.loads(r.data.decode('utf-8'))
+    print('Getting Quote, Response --->', r.status)
     rand = random.randint(0, 1600)
     quote = dec_resp[rand]['text']
     author = dec_resp[rand]['author']
 
     msg = f"\n\n*{quote}*\n~ {author}\n\n"
+
     return msg
 
 
@@ -83,5 +90,6 @@ def create_wish():
     userList, nameList = check_users(token)
     msg = f'{quote}{covid}'
     send_wish(msg, userList, nameList, token)
+
 
 create_wish()
