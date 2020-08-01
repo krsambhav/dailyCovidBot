982bf54f922cda68b9271d14566a6f93561634a7
Kumar Sambhav <krsambhav16@gmail.com>
2020-08-01 07:51:53 +0530

    .

--- wish.py
+++ wish.py
@@ -1,23 +1,19 @@
 import time
 import json
-import requests
+import urllib3
 import random
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
-# def send_wish(msg):
-#     token = '1308060193:AAFkI1FAWd5k_Wf7Vk6sSzAgXVCdEg7lIeY'
-#     chat_id = '954226967'
-
-#     send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + msg
-
-#     response = requests.get(send_text)
+http = urllib3.PoolManager()
 
 # testBot = 1392096396:AAGwZfy5fk0bywDGNYtSolMDYV7MafAGJOY
 # covidBot = 1308060193:AAFkI1FAWd5k_Wf7Vk6sSzAgXVCdEg7lIeY
 
 def check_users(token):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
-    response = requests.get(url)
-    dec_resp = response.json()
+    r = http.request('GET', url)
+    dec_resp = json.loads(r.data.decode('utf-8'))
     lenId = len(dec_resp['result'])
     userArr = []
     nameArr = []
@@ -36,44 +32,44 @@ def send_wish(msg, userList, nameList, token):
     listLen = len(userList)
     for x in range(0, listLen):
         send_text = 'https://api.telegram.org/bot' + token + \
-        '/sendMessage?chat_id=' + str(userList[x]) + '&parse_mode=Markdown&text=Good Morning ' + str(nameList[x]) + '!' + msg
-        response = requests.get(send_text)
+        '/sendMessage?chat_id=' + str(userList[x]) + '&parse_mode=markdown&text=*Good Morning ' + str(nameList[x]) + '!*' + msg
+        response = http.request('GET', send_text)
 
 
 def get_covid_data():
     url = 'https://api.covid19india.org/data.json'
-    response = requests.get(url)
-    dec_resp = response.json()
-    total_confirmed = dec_resp['statewise'][0]['confirmed']
-    total_active = dec_resp['statewise'][0]['active']
-    total_recovered = dec_resp['statewise'][0]['recovered']
-    total_deaths = dec_resp['statewise'][0]['deaths']
+    r = http.request('GET', url)
+    dec_resp = json.loads(r.data.decode('utf-8'))
+    total_confirmed = int(dec_resp['statewise'][0]['confirmed'])
+    total_active = int(dec_resp['statewise'][0]['active'])
+    total_recovered = int(dec_resp['statewise'][0]['recovered'])
+    total_deaths = int(dec_resp['statewise'][0]['deaths'])
 
-    daily_confirmed = dec_resp['cases_time_series'][-1]['dailyconfirmed']
-    daily_deceased = dec_resp['cases_time_series'][-1]['dailydeceased']
-    daily_recovered = dec_resp['cases_time_series'][-1]['dailyrecovered']
+    daily_confirmed = int(dec_resp['cases_time_series'][-1]['dailyconfirmed'])
+    daily_deceased = int(dec_resp['cases_time_series'][-1]['dailydeceased'])
+    daily_recovered = int(dec_resp['cases_time_series'][-1]['dailyrecovered'])
 
     msg = f"CoViD Updates:\n" \
-          f"Yesterday Confirmed: {daily_confirmed}\nYesterday Deceased: {daily_deceased}\nYesterday Recovered: {daily_recovered}\n\n" \
-          f"Total Confirmed: {total_confirmed}\nTotal Active: {total_active}\nTotal Recovered: {total_recovered}\nTotal Deaths: {total_deaths}" \
-          f"\n\nStay Home, Stay Safe!"
+          f"Yesterday Confirmed: *{daily_confirmed:n}*\nYesterday Recovered: *{daily_recovered:n}*\nYesterday Deceased: *{daily_deceased:n}*\n-----------------------------------------\n" \
+          f"Total Confirmed: *{total_confirmed:n}*\nTotal Active: *{total_active:n}*\nTotal Recovered: *{total_recovered:n}*\nTotal Deaths: *{total_deaths:n}*" \
+          f"\n\n*Stay Home, Stay Safe* \U0001f9e1"
     return msg
 
 
 def get_quote():
     url = 'https://type.fit/api/quotes'
-    response = requests.get(url)
-    dec_resp = response.json()
+    r = http.request('GET', url)
+    dec_resp = json.loads(r.data.decode('utf-8'))
     rand = random.randint(0, 1600)
     quote = dec_resp[rand]['text']
     author = dec_resp[rand]['author']
 
-    msg = f"\n\n{quote} ~ {author}\n\n"
+    msg = f"\n\n*{quote}*\n~ {author}\n\n"
     return msg
 
 
 def create_wish():
-    token = '1308060193:AAFkI1FAWd5k_Wf7Vk6sSzAgXVCdEg7lIeY'
+    token = '1392096396:AAGwZfy5fk0bywDGNYtSolMDYV7MafAGJOY'
     quote = get_quote()
     covid = get_covid_data()
     userList, nameList = check_users(token)
@@ -82,3 +78,5 @@ def create_wish():
 
 
 create_wish()
+
+
